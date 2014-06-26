cf183737cfbf61f59716ab2dd5b2778306353e31
UnitedJayMo <unitedjaymo@live.com>
2014-06-26 00:16:14 -0500

    Drew the Player, Looks preeeetty dull.

--- TileRealms/Player.cs
+++ TileRealms/Player.cs
@@ -15,11 +15,25 @@ namespace TileRealms
         Viewport viewport;
         Texture2D texture;
         Rectangle destRect;
-        Rectangle srcRect;
+        Rectangle srcRect; //48 x 48
+        Vector2 frames;
+        Vector2 location;
+        int size;
+
         public void Initialize(Viewport _vp)
         {
             // TODO: Add your initialization logic here
             viewport = _vp; //Viewport is the screen resolution. You can get height / width properties.
+            
+            if (viewport.Width > viewport.Height)
+                size = viewport.Height / 10;
+
+            else
+                size = viewport.Height / 10;
+
+            frames = new Vector2(0, 0);
+            destRect = new Rectangle((int)location.X, (int)location.Y, size, size);
+            srcRect = new Rectangle((int)frames.X, (int)frames.Y, 48, 48);
         }
 
         /// <summary>
@@ -29,7 +43,7 @@ namespace TileRealms
         public void LoadContent(ContentManager Content)
         {
             // TODO: use this.Content to load your game content here
-            texture = Content.Load<Texture2D>("Content\\Basic-spritesheet");
+            texture = Content.Load<Texture2D>("Basic-spritesheet");
         }
 
         /// <summary>
@@ -58,9 +72,7 @@ namespace TileRealms
         public void Draw(SpriteBatch spriteBatch)
         {
             // TODO: Add your drawing code here
-            spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend);
             spriteBatch.Draw(texture, destRect, srcRect, Color.White);
-            spriteBatch.End();
         }
     }
 }
