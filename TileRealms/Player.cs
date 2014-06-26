18c5dd9a9422f3c925d0e9f2574ad3e1164fe7e2
UnitedJayMo <UnitedJayMo@live.com>
2014-06-26 15:13:10 -0700

    Got preliminary animation done.

--- TileRealms/Player.cs
+++ TileRealms/Player.cs
@@ -14,29 +14,33 @@ namespace TileRealms
     {
         Viewport viewport;
         Texture2D texture;
+        Texture2D up;
+        Texture2D down;
+        Texture2D right;
+        Texture2D left;
+
         Rectangle destRect;
         Rectangle srcRect;
-        Vector2 frames;
+        int frames;
         public Vector2 location;
-        public int size;
+
+        float time = 0f;
 
         public void Initialize(Viewport _vp)
         {
             viewport = _vp;
-            
-            if (viewport.Width > viewport.Height)
-                size = viewport.Height / 10;
-            else
-                size = viewport.Width / 10;
 
-            frames = new Vector2(0, 0);
-            destRect = new Rectangle((int)location.X, (int)location.Y, size, size);
-            srcRect = new Rectangle((int)frames.X, (int)frames.Y, 48, 48);
+            destRect = new Rectangle((int)location.X, (int)location.Y, 64, 64);
+            srcRect = new Rectangle((int)frames, (int)0, 16, 16);
         }
 
         public void LoadContent(ContentManager content)
         {
-            texture = content.Load<Texture2D>("Random_warrior");
+            texture = content.Load<Texture2D>("player_up");
+            up = content.Load<Texture2D>("player_up");
+            down = content.Load<Texture2D>("player_down");
+            right = content.Load<Texture2D>("player_right");
+            left = content.Load<Texture2D>("player_left");
         }
 
         public void UnloadContent()
@@ -46,29 +50,47 @@ namespace TileRealms
 
         public void Update(GameTime gameTime)
         {
+            time += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (time > 200)
+            {
+                frames++;
+                if (frames >= 4)
+                    frames = 0;
+                time = 0f;
+            }
+
+            srcRect.X = frames * 16;
+            destRect.X = (int)location.X;
+            destRect.Y = (int)location.Y;
+
             KeyboardState state = Keyboard.GetState();
 
             if (state.IsKeyDown(Keys.W))
             {
+                texture = up;
                 location.Y -= 10;
             }
             if (state.IsKeyDown(Keys.S))
             {
+                texture = down;
                 location.Y += 10;
             }
             if (state.IsKeyDown(Keys.A))
             {
+                texture = left;
                 location.X -= 10;
             }
             if (state.IsKeyDown(Keys.D))
             {
+                texture = right;
                 location.X += 10;
             }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, location, srcRect, Color.White);
+            spriteBatch.Draw(texture, destRect, srcRect, Color.White);
         }
     }
 }
