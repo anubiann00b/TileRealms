010c18002783209bfd798476253e60e1674f0330
Shreyas <skraman1999@gmail.com>
2014-06-25 22:18:53 -0700

    Player can move! (not very well though).

--- TileRealms/Player.cs
+++ TileRealms/Player.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TileRealms
@@ -22,12 +23,10 @@ namespace TileRealms
 
         public void Initialize(Viewport _vp)
         {
-            // TODO: Add your initialization logic here
-            viewport = _vp; //Viewport is the screen resolution. You can get height / width properties.
+            viewport = _vp;
             
             if (viewport.Width > viewport.Height)
                 size = viewport.Height / 10;
-
             else
                 size = viewport.Height / 10;
 
@@ -36,43 +35,41 @@ namespace TileRealms
             srcRect = new Rectangle((int)frames.X, (int)frames.Y, 48, 48);
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         public void LoadContent(ContentManager Content)
         {
-            // TODO: use this.Content to load your game content here
             texture = Content.Load<Texture2D>("Random_warrior");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         public void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.W))
+            {
+                location.Y--;
+            }
+            if (state.IsKeyDown(Keys.S))
+            {
+                location.Y++;
+            }
+            if (state.IsKeyDown(Keys.A))
+            {
+                location.X--;
+            }
+            if (state.IsKeyDown(Keys.D))
+            {
+                location.X++;
+            }
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
-            // TODO: Add your drawing code here
-            spriteBatch.Draw(texture, destRect, srcRect, Color.White);
+            spriteBatch.Draw(texture, location, srcRect, Color.White);
         }
     }
 }
