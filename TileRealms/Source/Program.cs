0ed7c947791e4c702890554734c1a4451f0aee6e
Shreyas <skraman1999@gmail.com>
2014-06-30 23:47:38 -0700

    Put files in folders. Too much Java packaging... But it's better now.

--- TileRealms/Source/Program.cs
+++ TileRealms/Source/Program.cs
@@ -2,14 +2,8 @@
 
 namespace TileRealms
 {
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main()
         {
             var factory = new MonoGame.Framework.GameFrameworkViewSource<MainGame>();
