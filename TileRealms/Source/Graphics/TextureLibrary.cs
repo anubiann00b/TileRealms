3da2011a1373b8d7b894d3486bce03ad856fd883
Shreyas <skraman1999@gmail.com>
2014-07-02 19:55:52 -0700

    Added TextureLibrary, not finished.

---
+++ TileRealms/Source/Graphics/TextureLibrary.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileRealms.Source.Graphics
+{
+    class TextureLibrary
+    {
+        static List<TextureLibrary> textures = new List<TextureLibrary>();
+
+        static TextureLibrary PLAYER_DOWN = new TextureLibrary("player_down");
+
+        static void LoadLibraryContent(ContentManager content)
+        {
+            PLAYER_DOWN.LoadContent(content);
+            textures.Add(PLAYER_DOWN);
+        }
+
+        String path;
+        Texture2D texture;
+
+        private TextureLibrary(String res)
+        {
+            path = res;
+        }
+
+        void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>(path);
+        }
+    }
+}
