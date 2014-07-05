bb81debe446bf30285fbf67208c6168dba9a5940
Shreyas <skraman1999@gmail.com>
2014-07-04 20:14:00 -0800

    Events are so cool.

---
+++ TileRealms/Source/Network/ServerTCP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Networking.Sockets;
+
+namespace TileRealms
+{
+    class ServerTCP
+    {
+        StreamSocketListener socketListener;
+
+        public void Start()
+        {
+            socketListener = new StreamSocketListener();
+            IAsyncAction bindAction = socketListener.BindServiceNameAsync("9999");
+            Task.WaitAny(bindAction.AsTask());
+            socketListener.ConnectionReceived += ClientConnectionRecieved;
+        }
+
+        private void ClientConnectionRecieved(StreamSocketListener sl, StreamSocketListenerConnectionReceivedEventArgs eventArgs)
+        {
+            
+        }
+
+        private void HandleClientConnection()
+        {
+            
+        }
+    }
+}
