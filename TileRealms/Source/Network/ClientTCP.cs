d024b9ec9de4ad3f45778779c40a4513a386cdff
Shreyas <skraman1999@gmail.com>
2014-07-05 12:18:55 -0700

    Client and server connection. Need to be able to test it...

---
+++ TileRealms/Source/Network/ClientTCP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace TileRealms
+{
+    class ClientTCP
+    {
+        StreamSocket socket;
+        int id;
+
+        public void Start()
+        {
+            socket = new StreamSocket();
+            IAsyncAction bindAction = socket.ConnectAsync(new HostName("127.0.0.1"), "9999"); ;
+            Task.WaitAny(bindAction.AsTask());
+
+
+
+            Task.Run(() => Read());
+        }
+
+        public void Read()
+        {
+            IInputStream clientIn = socket.InputStream;
+            IOutputStream clientOut = socket.OutputStream;
+
+            DataWriter dataOut = new DataWriter(clientOut);
+            DataReader dataIn = new DataReader(clientIn);
+
+            id = dataIn.ReadInt32();
+
+            dataOut.WriteInt32(id);
+        }
+    }
+}
--- TileRealms/Source/Network/ServerTCP.cs
+++ TileRealms/Source/Network/ServerTCP.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
 
 namespace TileRealms
 {
     class ServerTCP
     {
         StreamSocketListener socketListener;
+        int clientCounter;
 
         public void Start()
         {
+            clientCounter = 0;
             socketListener = new StreamSocketListener();
             IAsyncAction bindAction = socketListener.BindServiceNameAsync("9999");
             Task.WaitAny(bindAction.AsTask());
             socketListener.ConnectionReceived += ClientConnectionRecieved;
         }
 
-        private void ClientConnectionRecieved(StreamSocketListener sl, StreamSocketListenerConnectionReceivedEventArgs eventArgs)
+        private void ClientConnectionRecieved(StreamSocketListener server, StreamSocketListenerConnectionReceivedEventArgs eventArgs)
         {
-            
-        }
+            StreamSocket client = eventArgs.Socket;
 
-        private void HandleClientConnection()
-        {
-            
+            IInputStream clientIn = client.InputStream;
+            IOutputStream clientOut = client.OutputStream;
+
+            DataWriter dataOut = new DataWriter(clientOut);
+            DataReader dataIn = new DataReader(clientIn);
+
+            Interlocked.Increment(ref clientCounter);
+            dataOut.WriteInt32(Interlocked.Increment(ref clientCounter));
+
+            Debug.WriteLine(dataIn.ReadInt32());
         }
     }
 }
